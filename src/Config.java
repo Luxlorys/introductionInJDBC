6e0e39ed326e50829744e2f1402bc210bf2cd89e
Luxlorys <bossbyrcev221@gmail.com>
2022-05-29 21:41:49 +0300

    create base structure

---
+++ src/Config.java
@@ -0,0 +1,6 @@
+public class Config {
+    static String URL = "jdbc:mysql://localhost:3306/db";
+    static String USER = "gromozeqa";
+    static String PASSWORD = "password#";
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
+import java.sql.*;
 
 public class Main {
-    public static void main(String[] args){
 
+    static String URL = Config.URL;
+    static String USER = Config.USER;
+    static String PASSWORD = Config.PASSWORD;
+    public static void main(String[] args) throws SQLException{
+        select();
     }
+
+    private static Connection connection() throws SQLException{
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        return connection;
+    }
+    private static boolean select() throws SQLException{
+        try{
+            Statement statement = connection().createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM User");
+
+            while (rs.next()){
+                System.out.print("Id: " + rs.getInt("id") + " | ");
+                System.out.print("Login: " + rs.getString("login") + " | ");
+                System.out.println("Password: " + rs.getString("password") + " | ");
+
+            }
+        } catch (Exception e){
+            e.getMessage();
+        }
+        return true;
+    }
+
+    private static boolean insert(String login, String password){return true;}
+
+    private static boolean delete(String login){return true;}
+
+    private static boolean update(String login, String password){return true;}
 }
+
 
 
