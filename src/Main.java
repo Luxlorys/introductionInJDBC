81287072715cc1f43397155060a1b87aeeab8f5c
luxlorys <bossbyrcev221@gmail.com>
2022-06-02 19:56:21 +0300

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -5,76 +5,81 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 
-import config.Config.*;
-
 public class Main {
 
     static String URL = Config.getURL();
     static String USER = Config.getUSER();
     static String PASSWORD = Config.getPASSWORD();
-    public static void main(String[] args) throws SQLException{
+
+    public static void main(String[] args) throws SQLException {
 //        insert("andrew3", "password##");
 ////        select();
 //        delete("test5");
         select();
     }
 
-    private static Statement getStatement() throws SQLException{
-        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        Statement statement = connection.createStatement();
-        return statement;
+    private static Statement getStatement() throws SQLException {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement statement = connection.createStatement();
+            return statement;
+        } catch (SQLException exception) {
+            return null;
+        }
     }
 
-    private static boolean select() throws SQLException{
-        try{
+    private static boolean select() throws SQLException {
+        try {
             ResultSet rs = getStatement().executeQuery("SELECT * FROM User");
 
-            while (rs.next()){
+            while (rs.next()) {
                 System.out.print("Id: " + rs.getInt("id") + " | ");
                 System.out.print("Login: " + rs.getString("login") + " | ");
                 System.out.println("Password: " + rs.getString("password") + " | ");
 
             }
-        } catch (SQLException exception){
+        } catch (SQLException exception) {
             exception.printStackTrace();
         }
         return true;
     }
 
 
-    private static boolean insert(String login, String password){
-        try{
+    private static boolean insert(String login, String password) {
+        try {
             String query = "INSERT INTO User (login, password) VALUES (\"" + login + "\", \"" + getPasswordHash(password) + "\")";
             getStatement().executeUpdate(query);
-        } catch (SQLException exception){
+        } catch (SQLException exception) {
             exception.printStackTrace();
         }
         return true;
     }
 
 
-    private static boolean delete(String login){
+    private static boolean delete(String login) {
         try {
             String query = "DELETE FROM User WHERE login = " + "\"" + login + "\"";
             getStatement().executeUpdate(query);
-        } catch (SQLException exception){
+        } catch (SQLException exception) {
             exception.printStackTrace();
         }
         return true;
     }
 
 
-    private static boolean update(String login, String password){return true;}
+    private static boolean update(String login, String password) {
+        return true;
+    }
 
 
-    private static String getPasswordHash(String password){
-        try{
+    private static String getPasswordHash(String password) {
+        try {
             MessageDigest md = MessageDigest.getInstance("SHA-256"); // not recommended to use
             byte[] digest = md.digest(password.getBytes());
             final String hash = new BigInteger(1, digest).toString(16);
 
             return hash;
-        } catch(NoSuchAlgorithmException e){
+        } catch (NoSuchAlgorithmException e) {
             System.err.println("Error");
         }
         return "";
