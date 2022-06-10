1795e708bf06e1d783a3b4c370eadf429af1d9ae
luxlorys <bossbyrcev221@gmail.com>
2022-06-10 16:15:52 +0300

    changed hashing method

--- src/Hashing.java
+++ src/Hashing.java
@@ -1,17 +1,20 @@
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class Hashing {
 
 
-    public final String getSecurePassword(String password) {
+    public final String[] getSecurePassword(String password) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256"); // not recommended to use
             byte[] digest = md.digest(password.getBytes());
             final String hash = new BigInteger(1, digest).toString(16);
 
-            return hash + getSalt();
+            String salt = getSalt();
+
+            return new String[]{hash + salt, salt};
         } catch (NoSuchAlgorithmException exception) {
             throw new RuntimeException(exception);
         }
@@ -19,11 +22,17 @@ public class Hashing {
 
 
     // ok, I will change code later
-    public static int getSalt() {
-        int max = 9999;
-        int min = 1000;
+    public static String getSalt() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
 
-        return (int) Math.floor(Math.random() * (max - min + 1) + min); // random number from min to max
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 
 }
